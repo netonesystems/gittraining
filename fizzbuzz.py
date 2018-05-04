d60b497a3bb2e219d0992a4d869617142648622e
Go Chiba <go.chiba@gmail.com>
2018-05-04 18:10:28 +0900

    Add sample code

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+
+"""
+Main funtion
+"""
+if __name__ == "__main__":
+  try:
+  end = int(sys.argv[1])
+    check_num(end)
+  except TypeError:
+    sys.stdout.write("Please try with Interger argument\n")
---
+++ num_checker.py
@@ -0,0 +1,13 @@
+"""
+check provided number
+"""
+def check_num(num):
+  for x in range(1,num):
+    if (x%15 == 0):
+      sys.stdout.write("FizzBuzz\n")
+    elif (x%3 == 0):
+      sys.stdout.write("Fizz\n")
+    elif (x%5 == 0):
+      sys.stdout.write("Bazz\n")
+    else:
+      sys.stdout.write("%i\n" % (x,))
