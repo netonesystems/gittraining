792caba01a8eb627fcd1e0b9930ab179a738eaf3
kodaken <k-kodama@netone.co.jp>
2018-05-07 12:43:20 +0900

    Fixed issue #3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -7,7 +7,7 @@ Main funtion
 """
 if __name__ == "__main__":
   try:
-  end = int(sys.argv[1])
-    check_num(end)
+  	end = int(sys.argv[1])
+  	check_num(end)
   except TypeError:
-    sys.stdout.write("Please try with Interger argument\n")
+  	sys.stdout.write("Please try with Interger argument\n")
