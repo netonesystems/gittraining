e692ddc59bc40bcd5c82e1826a61246495e2fb3b
Yuta Nakayama <yt.nakayama0114@gmail.com>
2018-05-14 14:16:22 +0900

    modify fixtypo error

--- num_checker.py
+++ num_checker.py
@@ -4,13 +4,12 @@ check provided number
 import sys
 
 def check_num(num):
-    for x in range(1,num):
-        if (x%15 == 0):
+    for x in range(1, num):
+        if (x % 15 == 0):
             sys.stdout.write("FizzBuzz\n")
-        elif (x%3 == 0):
+        elif (x % 3 == 0):
             sys.stdout.write("Fizz\n")
-        elif (x%5 == 0):
+        elif (x % 5 == 0):
             sys.stdout.write("Buzz\n")
         else:
             sys.stdout.write("%i\n" % (x,))
-=======
